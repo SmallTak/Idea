1f8fcb5ccca63c338334109586dea63b9a9b9361
SmallTak <1048388981@qq.com>
2018-03-24 14:25:13 +0800

    二级缓存

--- mybatis/src/main/java/com/kaishengit/mapper/MovieMapper.java
+++ mybatis/src/main/java/com/kaishengit/mapper/MovieMapper.java
@@ -1,7 +1,17 @@
 package com.kaishengit.mapper;
 
+import com.kaishengit.entity.Movie;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
 public interface MovieMapper {
 
-
+    /*参数类型为String，参数最好加注解@param("title")*/
+    List<Movie> findByTitle(@Param("title") String title);
+    List<Movie> findByParams(Map<String, Object> params);
+    List<Movie> findByIdList(List<Integer> idList);
+    void addMovie(List<Movie> list);
 
 }
---
+++ mybatis/src/test/java/MovieMapperTest.java
@@ -0,0 +1,80 @@
+import com.kaishengit.entity.Movie;
+import com.kaishengit.mapper.MovieMapper;
+import com.kaishengit.util.SqlSessionFactoryUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class MovieMapperTest {
+
+    private SqlSession sqlSession;
+    private MovieMapper movieMapper;
+
+    @Before
+    public void intit(){
+
+        sqlSession = SqlSessionFactoryUtil.getSqlSession(true);
+        movieMapper = sqlSession.getMapper(MovieMapper.class);
+    }
+    @After
+    public void destory(){
+        sqlSession.close();
+    }
+
+    @Test
+    public void findByTitle(){
+
+        List<Movie> movieList = movieMapper.findByTitle("%大话西游%");
+        for (Movie movie : movieList){
+            System.out.println(movie);
+        }
+
+    }
+    @Test
+    public void findByIdList(){
+
+        List<Integer> idList = Arrays.asList(123,321,222);
+        List<Movie> movieList = movieMapper.findByIdList(idList);
+        for (Movie movie : movieList){
+            System.out.println(movie);
+        }
+    }
+
+    @Test
+    public  void findByTitleAndDirector(){
+        String title = "";//"%大话西游%";
+        String director = "%周星驰%";
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("title", title);
+        params.put("director", director);
+        List<Movie> movieList = movieMapper.findByParams(params);
+        for (Movie movie : movieList){
+            System.out.println(movie);
+        }
+    }
+
+    @Test
+    public void addMovie(){
+        List<Movie> list = new ArrayList<>();
+        Movie movie = new Movie();
+        movie.setTitle("千与千寻");
+        movie.setDirector("宫崎骏");
+        movie.setRate(9.1F);
+
+        Movie movie2 = new Movie();
+        movie2.setTitle("龙猫");
+        movie2.setDirector("宫崎骏");
+        movie2.setRate(8.9F);
+        list.add(movie);
+        list.add(movie2);
+
+        movieMapper.addMovie(list);
+    }
+
+
+}
