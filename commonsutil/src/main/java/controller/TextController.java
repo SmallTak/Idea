f4b58ce4bef0a90b19067828792d61f87e82d6e7
SmallTak <1048388981@qq.com>
2018-05-20 22:25:54 +0800

    完成持续集成

---
+++ commonsutil/src/main/java/controller/TextController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TextController extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+}
